bc37df0c57f7cbd148b52b9db9b0165d60d47cad
Jim Kennedy <jim8786453@gmail.com>
2014-11-19 22:03:02 +0000

    Search
    - Improved variable naming
    - Removed debugging statements

--- src/search.lisp
+++ src/search.lisp
@@ -12,38 +12,36 @@
            :search-objs))
 (in-package :json-serve.search)
 
-(defun count-objs (filter query)
+(defun count-objs (category free-text)
   "Perform a search on the Mongodb document collection."
-  (print query)
   (db.use (config :database))
   (let ((results (docs (db.count (config :collection)
-                                 (generate-json-query filter query)))))
+                                 (build-query category free-text)))))
     (doc-to-ht (car results))))
 
-(defun search-objs (skip filter query)
+(defun search-objs (skip category free-text)
   "Perform a search on the Mongodb document collection."
-  (print (generate-json-query filter query))
   (db.use (config :database))
   (let ((docs (docs (db.sort (config :collection)
-                             (generate-json-query filter query)
+                             (build-query category free-text)
                              :limit 20
                              :skip skip
                              :field (config :title)))))
     (loop for doc in docs
        collect (doc-to-ht doc))))
 
-(defun generate-json-query (filter query)
-  (if (and (not (null filter)) (not (null query)))
-      (return-from generate-json-query
+(defun build-query (category free-text)
+  (if (and (not (null category)) (not (null free-text)))
+      (return-from build-query
         (kv
-         (kv (config :filter) filter)
-         (kv "$text" (kv "$search" query)))))
-  (if (not (null filter))
-      (return-from generate-json-query
-        (kv (config :filter) filter)))
-  (if (not (null query))
-      (return-from generate-json-query
-        (kv "$text" (kv "$search" query))))
+         (kv (config :category) category)
+         (kv "$text" (kv "$search" free-text)))))
+  (if (not (null category))
+      (return-from build-query
+        (kv (config :category) category)))
+  (if (not (null free-text))
+      (return-from build-query
+        (kv "$text" (kv "$search" free-text))))
   :all)
 
 (defun doc-to-ht (doc)
